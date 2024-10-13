964e0c902b701ba05cb6966384d89036daf9cee5
Bisma-Waseem <bwartofficial88@gmail.com>
2024-10-13 23:39:03 +0500

    Add files via upload

---
+++ appn.js
@@ -0,0 +1,203 @@
+// -----------Q 6 TO 9----------------
+// Q 1
+// let a =10
+// document.write(`The value of a is ${a} <br>`)
+// document.write(`....................<br>`)
+// document.write(`THe value of ++a is : ${++a} <br>`)
+// document.write(`Now the value of a is :${a} <br>`)
+
+// document.write(`The value of a++ is ${a++} <br>`)
+// document.write(`Now the value of a is  ${a} <br>`)
+
+// document.write(`The value of --a is : ${--a} <br>`)
+// document.write(`Now the value of a is ${a} <br>`)
+
+// document.write(`the value of a-- is:${a--} <br>`)
+// document.write(`Now the value of a-- is ${a}`)
+
+// Q 2
+// let a=2
+// let b=1
+// document.write(`--a is ${--a} <br>`)
+// document.write(`--a - --b is ${--a - --b} <br>`)
+// document.write(`--a - --b + ++b is ${--a - --b + ++b} <br>`)
+//  document.write(`--a - --b + ++b + b-- is ${--a - --b + ++b + b--} <br>`)
+//  document.write(`a is ${a} <br>`)
+//  document.write(`b is ${b} <br>`)
+//  document.write(`The result is ${--a - --b + ++b + b--}`)
+
+// Q 3
+// prompt("Salaam! What's your name")
+// {console.log("welcome to this site");
+// }
+
+// Q 4
+//   let number=+prompt("give a number",5)
+//   for(var i=1; i<=10; i++){
+//      let answer=number*i;
+//     document.write(`${number} * ${i} = ${answer} <br>`);
+//   }
+
+// Q 5
+// let sub1=prompt("write a  name of subject one")
+//  let obtained1=prompt("write obtained marks of subject one")
+//  let sub2=prompt("write a name of subject two")
+//  let obtained2=prompt("write obtained marks of subject two")
+//  let sub3=prompt("write a  name of subject three")
+//  let obtained3=prompt("write a obtained mrks of subject three")
+// let totalmarks=100
+//  document.write(`<h5>Subject  Total marks Obtained marks Percentage </h5> <br>`)
+
+// document.write(`${sub1} &nbsp  &nbsp &nbsp &nbsp${totalmarks} &nbsp &nbsp &nbsp &nbsp ${obtained1} &nbsp &nbsp &nbsp &nbsp${obtained1/totalmarks*100}% <br>`)
+
+//  document.write(`${sub2} &nbsp  &nbsp &nbsp &nbsp${totalmarks} &nbsp &nbsp &nbsp &nbsp ${obtained2} &nbsp &nbsp &nbsp &nbsp${obtained2/totalmarks*100}% <br>`)
+// document.write(`${sub3} &nbsp  &nbsp &nbsp &nbsp${totalmarks} &nbsp &nbsp &nbsp &nbsp ${obtained3} &nbsp &nbsp &nbsp &nbsp${obtained3/totalmarks*100}%`)
+
+
+// ----------chap 9 to 11--------------
+// Q 1
+// let cityname=prompt("what's your city name");
+// if (cityname==="karachi") {console.log("welcome to city of lights");   
+// }
+// let gender=prompt("what's your gender");
+// if (gender == "male") {console.log("Good Morning Sir");
+// }
+// else{
+//     (gender == "female");
+//     console.log("Good Morning Madam");
+    
+// }
+// let signal=prompt("write trafic signal color");
+// if (signal == "red") {console.log("Must Stop");
+// }
+// if   (signal=="green")
+//     {console.log("Move now");
+
+//     }
+
+// else{
+//     (signal == "yellow");
+//     console.log("Ready to move");
+// }
+// Q 4
+// let fuel=prompt("what your fuel in car");
+// if (fuel <="0.25 ") {console.log("Please refill the fuel in your car");
+
+// }
+
+
+
+// // Q 6
+// let marks= prompt("whats your marks in physics")
+// if (marks >= "80") {console.log("Excellent your Grade is A-one");
+
+    
+//  }
+// Q 7
+//   let nmb1= prompt("Guess the number");
+// if (nmb1==5)   {console.log("bingo! Correct answer")};
+
+//  if (nmb1 ==6)  {console.log("Close enough to the correct number");
+//  }
+// let a = 4
+// if (++a===5) {alert("given condition for a variable is true")
+// }
+// let b=82
+// if (b++ ===83) {alert("given condition for variable b is true")
+// }
+// let c=12
+// if (c++ ===13) {alert("condition 1 is true")
+// }
+// if (c===13) {alert("condition 2 is true")
+    
+// }
+// if (++c <14) {alert("condition 3 is true")
+    
+// }
+// if (c===14) {alert("condition 4 is true")
+    
+// }
+// // d
+// let metarialCost = 20000
+// let labourCost = 2000
+// let totalCost = metarialCost+labourCost
+// if (totalCost===labourCost+metarialCost) {alert("The cost equals")
+    
+// }
+// // e
+// if (true) {alert("true")
+    
+// }
+// if (false) {alert("false")
+// }
+// // f
+// if ("car" < "car") {alert("car is smaller than car")
+    
+// }
+// what's true conditions in mentioned output
+// a'condition
+// c's condition 2 & 4
+// d's condition
+// e's true condition
+// 
+
+// Q 6
+// MARK SHEET
+// let obtained1=prompt("write obtained marks of subject one")
+// //  let sub2=prompt("write a name of subject two")
+//   let obtained2=prompt("write obtained marks of subject two")
+// //  let sub3=prompt("write a  name of subject three")
+//   let obtained3=prompt("write a obtained mrks of subject three")
+// let totalmarks=300
+// Q 9
+let num=prompt("give a number")
+if (num) {alert("its even number")
+    
+}
+else{
+    alert("its odd number")
+}
+
+
+
+// Q 11
+// 
+// let num1 = +prompt("Enter a number 1");
+// let opration = prompt("Enter a opration");
+// let num2 = +prompt("Enter number 2");
+
+// if (opration == "+") {
+//     alert(num1+num2);
+// }
+// else if (opration =="-") {
+//     alert(num1-num2);
+// }
+// else if (opration == "*") {
+//     alert(num1*num2);
+// }
+// else if (opration == "/") {
+//     alert(num1/num2);
+// }
+// else{
+//     alert("invalid opration")
+// }
+
+
+
+// Q 10
+// let T=prompt("what's temperature today")
+// if (T > 40) {console.log
+// ("its too hot outside")
+    
+// }
+// if (T > 30) {console.log
+// ("The weather today is normal")
+    
+// }
+// if (T > 20) {console.log
+// ("Today weather is cool ")
+// }
+// if (T > 10) {console.log
+// ("Today's weather is so cool")
+    
+// }
